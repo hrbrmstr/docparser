e914bbacab3979ddacda10987d66d4fede7c7159
boB Rudis <bob@rud.is>
2018-12-22 13:57:50 -0500

    R package repo initialization complete

---
+++ R/docparser-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/docparser>
+#' - BugReports: <https://gitlab.com/hrbrmstr/docparser/issues>
+#' 
+#' @md
+#' @name docparser
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
